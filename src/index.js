eda9002e4052b999ec93a5d8c6d7c0a31a4b0301
Yan <yancertuche@gmail.com>
2021-03-29 01:14:25 -0500

    adding text to readme file

--- src/index.js
+++ src/index.js
@@ -13,13 +13,14 @@ var port = process.env.PORT || 3525;
 // Convierte una petición recibida (POST-GET...) a objeto JSON
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
-app.use(cors({origin: '*'}));
 
+app.use(cors({origin: '*'}));
 app.get('/movies/:token', function(req, res){
     let token = req.params.token;
     console.log(token)
     if(token === Akelab){
 	    res.status(200).send(words);
+        console.log("se envió")
     }
     else{
         res.status(201).send({messege : 'need token'})
